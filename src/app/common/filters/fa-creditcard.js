72481dca18cb2a9798afb4817ebd1fc56a273b56
Alexa Rarick <ararick@four51.com>
2018-06-19 15:47:30 -0500

    fix: conditional check for credit card type before filter

--- src/app/common/filters/fa-creditcard.js
+++ src/app/common/filters/fa-creditcard.js
@@ -5,28 +5,32 @@ angular.module('orderCloud')
 function faCreditCard() {
     return function(type) {
         var result = 'fa-credit-card-alt';
-        switch(type.toLowerCase()) {
-            case 'visa':
-                result = 'fa-cc-visa';
-                break;
-            case 'mastercard':
-                result = 'fa-cc-mastercard';
-                break;
-            case 'american express':
-                result = 'fa-cc-amex';
-                break;
-            case 'diners club':
-                result = 'fa-cc-diners-club';
-                break;
-            case 'discover':
-                result = 'fa-cc-discover';
-                break;
-            case 'jcb':
-                result = 'fa-cc-jcb';
-                break;
-            default:
-                result = 'fa-credit-card-alt';
+        if (type) {
+            switch(type.toLowerCase()) {
+                case 'visa':
+                    result = 'fa-cc-visa';
+                    break;
+                case 'mastercard':
+                    result = 'fa-cc-mastercard';
+                    break;
+                case 'american express':
+                    result = 'fa-cc-amex';
+                    break;
+                case 'diners club':
+                    result = 'fa-cc-diners-club';
+                    break;
+                case 'discover':
+                    result = 'fa-cc-discover';
+                    break;
+                case 'jcb':
+                    result = 'fa-cc-jcb';
+                    break;
+                default:
+                    result = 'fa-credit-card-alt';
+            }
+            return result;
+        } else {
+            return result;
         }
-        return result;
-    }
+    };
 }
