90cbc7b5c3a1b75c4520a49d47ba5d8cb259bfd0
BrightMahi <mahindar.singh@brighthat.io>
2018-11-19 20:22:25 +0530

    Added Lakeshore Learning vendor shipping charges.

--- src/app/checkout/shipping/checkout.shipping.rates.js
+++ src/app/checkout/shipping/checkout.shipping.rates.js
@@ -330,6 +330,26 @@ function VendorShippingCriteria() {
 			name: 'YourDollar',
 			minOrderAmount: 0,
 			shippingCostFunc: function(order) {return 0;}
+		},
+		{
+			name: 'Lakeshore Learning',
+			minOrderAmount: 0,
+			shippingCostFunc: function(order) {
+				var fifteenPStates = ["AL", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "ID",
+						      "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI",
+						      "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY",
+						      "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN",
+						      "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"];
+				var twentyPStates = ["AK", "HI", "AS", "FM", "GU", "MH", "MP", "PW", "PR", "VI"]; 
+				
+				if(fifteenPStates.indexOf(order.state.toUpperCase()) > -1){
+					return order.amount*0.15;
+				}
+				else if(twentyPStates.indexOf(order.state.toUpperCase()) > -1){
+					return order.amount*0.20;
+				}
+				return 0;
+			}
 		}
 	];
 
@@ -433,7 +453,8 @@ function VendorShippingCriteria() {
 
         var shippingCost = shippingCalculator({
             amount: amount,
-            itemCount: itemCount
+            itemCount: itemCount, 
+	    state: state
         });
         return shippingCost;
     };
